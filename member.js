43318519b10efd9a5d41c95fa68f9d6e597077ab
Tania-Amanda Nkoyo Fredrick Eneye <38540885+crystal4000@users.noreply.github.com>
2025-04-15 16:34:05 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,16 @@
+function skillsMember() {
+  return (
+    <div className="skills-member">
+      <h2>Skills</h2>
+      <ul>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Node.js</li>
+        <li>CSS</li>
+        <li>HTML</li>
+      </ul>
+    </div>
+  );
+}
+export default skillsMember;
+// This component is a simple React functional component that renders a list of skills.
